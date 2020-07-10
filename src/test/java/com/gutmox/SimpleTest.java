049d0c16e29f69aaa405a07361065ff6b34427bb
Pablo Gutierrez <gutmox@gmail.com>
2020-07-10 21:44:52 +0200

    Plus a simple test

---
+++ src/test/java/com/gutmox/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.gutmox;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleTest {
+
+	@Test
+	void should_do_parse_it_all(){
+
+		assertThat(true).isTrue();
+	}
+}
