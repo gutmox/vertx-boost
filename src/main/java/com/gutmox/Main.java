f1439127753c2fbc9f65e4c1514a582af2f393c2
Pablo Gutierrez <gutmox@gmail.com>
2021-08-04 19:52:59 +0200

    Removing event bus options & fixing initialising with VertxOptions

--- src/main/java/com/gutmox/Main.java
+++ src/main/java/com/gutmox/Main.java
@@ -5,7 +5,6 @@ import io.reactivex.Single;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.eventbus.EventBusOptions;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.reactivex.core.Vertx;
@@ -28,7 +27,7 @@ public class Main {
 
 	public static Single<String> start(DeploymentOptions deploymentOptions) {
 		DefaultChannelId.newInstance();
-		VertxOptions vertxOptions = new VertxOptions().setEventBusOptions(getEventBusOptions());
+		VertxOptions vertxOptions = new VertxOptions();
 		vertxOptions.setEventLoopPoolSize(5);
 		vertxOptions.setWorkerPoolSize(1);
 		vertxOptions.setInternalBlockingPoolSize(1);
@@ -38,21 +37,9 @@ public class Main {
 		});
 	}
 
-	private static EventBusOptions getEventBusOptions() {
-		var eventBusOptions = new EventBusOptions();
-		var clusterEnable = Boolean.parseBoolean(System.getenv("CLUSTER_ENABLE"));
-		if (clusterEnable) {
-			var podIp = System.getenv("MY_POD_IP");
-			eventBusOptions.setClustered(clusterEnable);
-			eventBusOptions.setHost(podIp);
-			LOGGER.info("POD IP {0}", podIp);
-		}
-		return eventBusOptions;
-	}
-
 	private static Single<String> run(Class<? extends AbstractVerticle> verticle, VertxOptions options,
 		DeploymentOptions deploymentOptions) {
-		final Vertx vertx = Vertx.vertx();
+		final Vertx vertx = Vertx.vertx(options);
 		return vertx.rxDeployVerticle(verticle.getName(), deploymentOptions);
 	}
 }
